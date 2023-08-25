483f431fd603686f28f38f06b6d9dc30ff21cea1
pedrohenrique7799 <131817298+pedrohenrique7799@users.noreply.github.com>
2023-08-25 17:37:23 -0400

    atualização do codigo e desafio concluido

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -4,23 +4,24 @@ import { Link } from "react-router-dom";
 export default function ToDo() {
    const [atividade, setAtividade] = useState("");
    const [lista, setLista] = useState([]);
-
+   const [id, setId] = useState(1);
    const salvar = (e) =>{
     e.preventDefault();
-    setLista([...lista, {atividade:atividade}]);
+    setLista([...lista, {atividade:atividade, id:id}]);
     console.log(lista)
+    setId = (id+1);
    };
     return (
         <div>
             <h1>Lista de Atividades</h1>
-            <p>{Atividade}</p>
+            <p>{atividade}</p>
             <Link to="/">home</Link>
            <form onSubmit={salvar}>
             <input type="text"
             onChange={(e) => setAtividade(e.target.value)}/>
             <button>ADD</button>
            </form>
-           {lista.map((atividade) => <p>{atividade.atividade}</p>)}
+           {lista.map((atividade) => <p key={atividade.id}>{atividade.atividade}</p>)}
         </div>
     );
 }
