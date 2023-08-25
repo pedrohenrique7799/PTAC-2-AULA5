fed2abaa4638d7e28b01c6882de13181a551a7f2
pedrohenrique7799 <131817298+pedrohenrique7799@users.noreply.github.com>
2023-08-25 16:34:08 -0400

    adicionado as const

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   
+   const [atividade, setAtividade] = useState("");
+   const [lista, setLista] = useState([]);
     return (
         <div>
             <h1>Lista de Atividades</h1>
