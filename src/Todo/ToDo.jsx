740f0390241a85e0acaf950cd5113369d6e32ba5
pedrohenrique7799 <131817298+pedrohenrique7799@users.noreply.github.com>
2023-09-05 09:50:22 -0400

    alterando parametro para o desejado

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -3,27 +3,28 @@ import { Link } from "react-router-dom";
 import './style.css'
 
 export default function ToDo() {
-   const [atividade, setAtividade] = useState("");
+   const [nome, setNome] = useState("");
    const [lista, setLista] = useState([]);
    const [id, setId] = useState(1);
    const salvar = (e) =>{
     e.preventDefault();
-    setLista([...lista, {atividade:atividade, id:id}]);
+    setLista([...lista, {nome:nome, id:id}]);
     console.log(lista)
     setId = (id+1);
    };
     return (
         <div class="p">
-            <h1 id="gus">Lista de Atividades</h1>
+            <h1 id="gus">Lista de jogadores</h1>
             
-            <p>{atividade}</p>
+            <p>{nome}</p>
             <Link to="/" id="gus">home</Link>
            <form onSubmit={salvar}>
             <input type="text" id="gus"
-            onChange={(e) => setAtividade(e.target.value)}/>
+            onChange={(e) => setNome(e.target.value)}/>
             <button id="gus">ADD</button>
+            <input></input>
            </form>
-           {lista.map((atividade) => <p key={atividade.id}>{atividade.atividade}</p>)}
+           {lista.map((nome) => <p key={nome.id}>{nome.nome}</p>)}
         </div>
     );
 }
