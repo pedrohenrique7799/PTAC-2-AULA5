8ce8f8c0b2d22bc4b5763d189081247ed4f9c85f
pedrohenrique7799 <131817298+pedrohenrique7799@users.noreply.github.com>
2023-09-05 09:59:47 -0400

    criado use state para idade do jogador

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import './style.css'
+import "./style.css";
 
 export default function ToDo() {
-   const [nome, setNome] = useState("");
-   const [lista, setLista] = useState([]);
-   const [id, setId] = useState(1);
-   const salvar = (e) =>{
+  const [nome, setNome] = useState([]);
+  const [lista, setLista] = useState([]);
+  const [idade, setIdade] = useState([])
+
+  const [id, setId] = useState(1);
+  const salvar = (e) => {
     e.preventDefault();
-    setLista([...lista, {nome:nome, id:id}]);
-    console.log(lista)
-    setId = (id+1);
-   };
-    return (
-        <div class="p">
-            <h1 id="gus">Lista de jogadores</h1>
-            
-            <p>{nome}</p>
-            <Link to="/" id="gus">home</Link>
-           <form onSubmit={salvar}>
-            <input type="text" id="gus"
-            onChange={(e) => setNome(e.target.value)}/>
-            <button id="gus">ADD</button>
-            <input></input>
-           </form>
-           {lista.map((nome) => <p key={nome.id}>{nome.nome}</p>)}
-        </div>
-    );
+    setLista([...lista, { nome: nome, id: id }]);
+    console.log(lista);
+    setId = id + 1;
+  };
+  return (
+    <div class="p">
+      <h1 id="gus">Lista de jogadores</h1>
+
+      <p>{nome}</p>
+      <Link to="/" id="gus">
+        home
+      </Link>
+      <form onSubmit={salvar}>
+        <input type="text" id="gus" onChange={(e) => setNome(e.target.value)} />
+        <button id="gus">ADD</button>
+        <input></input>
+      </form>
+      {lista.map((nome) => (
+        <p key={nome.id}>{nome.nome}</p>
+      ))}
+    </div>
+  );
 }
